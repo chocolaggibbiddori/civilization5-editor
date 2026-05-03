bca7a30f1105a2c996b4bb44757384b8f6cb200c
chocola <didrkd684@naver.com>
2026-05-03 19:44:25 +0900

    [Test] Githook 테스트

--- src/main/java/chocola/civilizationfiveeditor/Launcher.java
+++ src/main/java/chocola/civilizationfiveeditor/Launcher.java
@@ -3,6 +3,7 @@ package chocola.civilizationfiveeditor;
 import javafx.application.Application;
 
 public class Launcher {
+
     public static void main(String[] args) {
         Application.launch(MainApplication.class, args);
     }
