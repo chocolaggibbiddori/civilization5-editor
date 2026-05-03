f16b9b5f27dd0767816ee0c6630aef3a4263236a
chocola <didrkd684@naver.com>
2026-05-03 20:03:16 +0900

    [Test] Githook 테스트

--- src/main/java/chocola/civilizationfiveeditor/MainController.java
+++ src/main/java/chocola/civilizationfiveeditor/MainController.java
@@ -1,14 +1,8 @@
 package chocola.civilizationfiveeditor;
 
-import chocola.civilizationfiveeditor.model.BuildingData;
-import chocola.civilizationfiveeditor.model.CivEntry;
-import chocola.civilizationfiveeditor.model.GameData;
-import chocola.civilizationfiveeditor.model.ImprovementData;
-import chocola.civilizationfiveeditor.model.TraitData;
-import chocola.civilizationfiveeditor.model.UnitData;
+import chocola.civilizationfiveeditor.model.*;
 import chocola.civilizationfiveeditor.service.XmlDataLoader;
 import chocola.civilizationfiveeditor.service.XmlDataSaver;
-
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -16,19 +10,11 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.IntConsumer;
-
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
